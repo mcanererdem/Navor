0c48c400d99676f614853b42c6594820ecda4a3e
cerdem <mcanererdem@gmail.com>
2022-09-19 08:59:50 +0300

    navigation and 2 fragments added

--- app/src/main/java/com/illaki/navigationtest1/FeedFragment.kt
+++ app/src/main/java/com/illaki/navigationtest1/FeedFragment.kt
@@ -1,10 +1,14 @@
 package com.illaki.navigationtest1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+
 
 class FeedFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,14 @@ class FeedFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_feedToCountry).setOnClickListener {
+            val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment(11)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
